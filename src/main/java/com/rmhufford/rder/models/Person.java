05badc177b7d1aca75074cc4bb544f52f2f6760b
Ryan Hufford <ryan@rmhufford.com>
2023-10-24 12:45:31 -0400

    getters required for serialization

--- src/main/java/com/rmhufford/rder/models/Person.java
+++ src/main/java/com/rmhufford/rder/models/Person.java
@@ -1,5 +1,6 @@
 package com.rmhufford.rder.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +13,27 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
 }
